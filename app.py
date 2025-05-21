c9aa9ec3c6069b3e56f5341a144ec08a48905e3c
annaandmandy <annaandtommy1004@gmail.com>
2025-05-21 12:40:13 -0400

    add advisors

--- app.py
+++ app.py
@@ -1,38 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
 import os
 import logging
+from data import PROJECTS_DATA, TEAM_MEMBERS, ADVISORS
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
-
-PROJECTS_DATA = [
-    {
-        "title": "Digitizing Centuries of History with Computer Vision",
-        "desc": "Developed a sophisticated computer vision pipeline to transcribe and digitize centuries-old handwritten records from the 18th century for the Hampden County Registry of Deeds. Our solution transformed 300-year-old documents into an easily searchable digital format, unlocking invaluable historical information.",
-        "link": "https://drive.google.com/file/d/1Fb0ZTVv_asP_Eh5YXzF9sgGjvfxtbmM0/view?usp=sharing"
-    },
-    {
-        "title": "AI-Powered Chest X-Ray Analysis for Enhanced Diagnostics",
-        "desc": "Developed a cutting-edge computer vision model for interpreting chest X-rays, achieving an impressive average AUC-ROC of 0.85 (ranking second on Papers With Code) and a leading macro F1 score of 0.39. This publicly accessible tool provides radiologists with an efficient and accurate aid for timely patient diagnoses.",
-        "link": "https://arxiv.org/abs/2505.06646"
-    },
-    {
-        "title": "Empowering Education Through Technology: Website and Database for Volunteer Tutors",
-        "desc": "Designed and built a comprehensive website and database platform for a volunteer organization dedicated to teaching underserved students. This platform streamlines operations, connects tutors with students, and facilitates effective learning.",
-        "link": "https://volunteertutors.netlify.app/"
-    },
-    {
-        "title": "Interactive Data Visualization for Federal Grant Distribution Analysis",
-        "desc": "Created an interactive web application for Senator Edward Markey’s Office to analyze over $10 billion in federal grant distributions across Massachusetts. This tool enables in-depth exploration and visualization of complex data, supporting informed decision-making and policy analysis.",
-        "link": "https://drive.google.com/file/d/1pGtc580Z6M3h_ymucOys10HG6rU7lgri/view?usp=sharing"
-    },
-]
-
-TEAM_MEMBERS = [{"name": "Daniel Strick", "role": "Founder / Lead Data Scientist", "image":'member_pics/danny.jpeg', "description":"I focus on designing and implementing sophisticated, data-driven solutions across various domains, translating intricate data challenges into tangible outcomes that boost efficiency and deliver strategic advantages for our clients.", "linkedIn":"https://www.linkedin.com/in/daniel-strick-512999116/"},
-                {"name": "Carlos Fernando Garcia Padilla", "role":"ML Engineer", "image":"member_pics/carlos.jpeg", "description":"I did my undergraduate at the University of California, San Diego with a major in Bioinformatics and a minor in Computer Science, and I am currently a Master student at Boston University studying Data Science.", "linkedIn":"https://www.linkedin.com/in/cfgp/"},
-                {"name": "Chuqiao (Carrie) Feng", "role":"AI Engineer", "image":"member_pics/carrie.jpg", "description":"hii", "linkedIn":"https://www.linkedin.com/in/chuqiao-feng/"},
-                {"name": "Hsiang Yu (Anna) Huang", "role":"Data Engineer", "image":"member_pics/anna.jpg", "description":"I graduated from NTUST with majors in Industrial Management and Finance and a minor in Computer Science, and I am currently pursuing a master's degree in Data Science at Boston University.", "linkedIn":"https://www.linkedin.com/in/hsiangyuhuang/"},]
 
 @app.route('/')
 def home():
@@ -96,7 +69,8 @@ def projects_with_slash():
 def team():
     team_information = "Our team is composed of highly skilled data scientists and AI engineers who thrive on solving complex challenges. Founder Daniel Strick serves as a Principal AI/ML Engineer, bringing deep expertise in machine learning, deep learning, and robust data engineering to Strick Data Solutions."
     return render_template('team.html', team_members=TEAM_MEMBERS,
-                           team_information = team_information)  # Use global TEAM_MEMBERS
+                           team_information = team_information,
+                           advisors = ADVISORS)  # Use global TEAM_MEMBERS
 @app.route('/team')
 def team_with_slash():
     return redirect(url_for('team'))
---
+++ data.py
@@ -0,0 +1,55 @@
+PROJECTS_DATA = [
+    {
+        "title": "Digitizing Centuries of History with Computer Vision",
+        "desc": "Developed a sophisticated computer vision pipeline to transcribe and digitize centuries-old handwritten records from the 18th century for the Hampden County Registry of Deeds. Our solution transformed 300-year-old documents into an easily searchable digital format, unlocking invaluable historical information.",
+        "link": "https://drive.google.com/file/d/1Fb0ZTVv_asP_Eh5YXzF9sgGjvfxtbmM0/view?usp=sharing"
+    },
+    {
+        "title": "AI-Powered Chest X-Ray Analysis for Enhanced Diagnostics",
+        "desc": "Developed a cutting-edge computer vision model for interpreting chest X-rays, achieving an impressive average AUC-ROC of 0.85 (ranking second on Papers With Code) and a leading macro F1 score of 0.39. This publicly accessible tool provides radiologists with an efficient and accurate aid for timely patient diagnoses.",
+        "link": "https://arxiv.org/abs/2505.06646"
+    },
+    {
+        "title": "Empowering Education Through Technology: Website and Database for Volunteer Tutors",
+        "desc": "Designed and built a comprehensive website and database platform for a volunteer organization dedicated to teaching underserved students. This platform streamlines operations, connects tutors with students, and facilitates effective learning.",
+        "link": "https://volunteertutors.netlify.app/"
+    },
+    {
+        "title": "Interactive Data Visualization for Federal Grant Distribution Analysis",
+        "desc": "Created an interactive web application for Senator Edward Markey’s Office to analyze over $10 billion in federal grant distributions across Massachusetts. This tool enables in-depth exploration and visualization of complex data, supporting informed decision-making and policy analysis.",
+        "link": "https://drive.google.com/file/d/1pGtc580Z6M3h_ymucOys10HG6rU7lgri/view?usp=sharing"
+    },
+]
+
+TEAM_MEMBERS = [
+    {
+        "name": "Daniel Strick",
+        "role": "Founder / Lead Data Scientist",
+        "image": "member_pics/danny.jpeg",
+        "description": "Daniel received his Master’s degree in Data Science from Boston University, where he also completed his undergraduate studies with a major in Economics and minors in Chemistry and Biology. He brings experience working with government and healthcare data.",
+        "linkedIn": "https://www.linkedin.com/in/daniel-strick-512999116/"
+    },
+    {
+        "name": "Carlos Fernando Garcia Padilla",
+        "role": "Co-founder / ML Engineer",
+        "image": "member_pics/carlos.jpeg",
+        "description": "Carlos completed his undergraduate studies at the University of California, San Diego, majoring in Bioinformatics with a minor in Computer Science. He is now pursuing a Master’s in Data Science at Boston University and is currently contributing to research in a bioinformatics lab.",
+        "linkedIn": "https://www.linkedin.com/in/cfgp/"
+    },
+    {
+        "name": "Chuqiao (Carrie) Feng",
+        "role": "Co-founder / AI Engineer",
+        "image": "member_pics/carrie.jpg",
+        "description": "Carrie holds a Bachelor's degree in Data Science from The Chinese University of Hong Kong, Shenzhen, and is now a Master’s student at Boston University. Her work focuses on developing and deploying AI-driven solutions.",
+        "linkedIn": "https://www.linkedin.com/in/chuqiao-feng/"
+    },
+    {
+        "name": "Hsiang Yu (Anna) Huang",
+        "role": "Co-founder / Data Engineer",
+        "image": "member_pics/anna.jpg",
+        "description": "Anna graduated from National Taiwan University of Science and Technology with a double major in Industrial Management and Finance and a minor in Computer Science. She is currently pursuing a Master’s in Data Science at Boston University. Her interests include sales forecasting and product analytics.",
+        "linkedIn": "https://www.linkedin.com/in/hsiangyuhuang/"
+    }
+]
+
+ADVISORS = [{"name": "Chris Seferlis", "role": "BI Advisor", "image":'member_pics/seferlis.jpeg', "description":"Chris has over 25 years’ experience in IT and a deep background in Data Warehousing and Business Intelligence. He is a Director of Technology Strategy at Microsoft, and a visiting Data Science Professor at Boston University.", "linkedIn":"https://www.linkedin.com/in/cseferlis/"},]
